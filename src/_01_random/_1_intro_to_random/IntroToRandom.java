ff356d643071bf53bc1daa98575525c7cc178d59
league <league@imac-29.attlocal.net>
2023-12-08 17:00:30 -0800

    ...

--- src/_01_random/_1_intro_to_random/IntroToRandom.java
+++ src/_01_random/_1_intro_to_random/IntroToRandom.java
@@ -12,14 +12,14 @@ public class IntroToRandom {
 		
 		// Before you can make random numbers, you need a Random object. Random objects make random things.
 		// Random objects are created like this.
-		Random ran = new Random();
+		Random ran = new Random(100);
 		
 		/*************  Random integers with no bounds  *************/
 		
 		// Run this program and you will see that nextInt created 5 random integers which have been printed to the console
 		for (int i=0; i<5; i++) {
 			number = ran.nextInt();
-			System.out.println(number);
+		//	System.out.println(number);
 		}
 		// The range of numbers that an int variable can hold is very large (as you can see in the console).
 		// Integers can also be positive or negative.
@@ -27,13 +27,13 @@ public class IntroToRandom {
 		/*************  Random positive integers with an upper limit only  *************/
 		
 		// Usually we want to limit the numbers to a range in our program. 		
-		for (int i=0; i<5; i++) {
+	//	for (int i=0; i<50; i++) {
 			// Uncomment the line of code below, but replace the _____ with a positive integer (e.g. 100) before running the program			
 
-			// number = ran.nextInt(_____);
-			System.out.println(number);
+		// number = ran.nextInt(10);
+		//	System.out.println(number);
 			
-		}
+		//}
 		
 		// All the numbers printed by this loop are less then the number you added - AND POSITIVE
 		// This means, if you entered 100, all the numbers returned by nextInt are between 0 and 99 inclusive
@@ -44,12 +44,12 @@ public class IntroToRandom {
 		// But what if you don't want the range to start at 0?  Let's say we want numbers between 25 and 75 inclusive.
 		// In this case, 25 is the lowest value we want, and 75 is the highest value. See below how to do this:
 		
-		for (int i=0; i<5; i++) {
+		for (int i=0; i<10; i += 1) {
 			// Uncomment the line of code below, and replace (_____)  with   (highest value - lowest value + 1)
 			// For our example, this will be:  ( 75 - 25 + 1 )
 			// Also replace <lowest value> with the start of the range.  For our example, this is 25.
 			
-			// number = ran.nextInt(_____) + <lowest value>;
+			 number = ran.nextInt(88- -222 +1) + -222;
 			System.out.println(number);
 			
 		}
